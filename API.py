637493d92f5f9671b6a0c9edeebafb68b398a264
marcelamelgar <61280126+marcelamelgar@users.noreply.github.com>
2021-04-23 20:53:46 -0600

    first  api

---
+++ API.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, url_for, redirect
+from jinja2 import Template, FileSystemLoader, Environment
+
+domain = "0.0.0.0:5000/"
+templates = FileSystemLoader('templates')
+environment = Environment(loader = templates)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def inicio():
+    return render_template("inicio.html")
+
+@app.route("/view", methods=["GET", "POST"])
+def view():
+    return render_template("view.html")
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    return render_template("create.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
