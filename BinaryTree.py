98df25a2aff88d56f7f1cb8e89a0da2658b1da79
marcelamelgar <61280126+marcelamelgar@users.noreply.github.com>
2021-04-25 17:44:57 -0600

    final tree

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,101 +1,76 @@
-from math import log
-class Node:
-    def __init__(self, equipo1, equipo2):
-        self.data = None
-        self.left_child = equipo1
-        self.right_child = equipo2
+class Nodes:
+  def __init__(self, data):
+    self.data = data
+    self.left_child = None
+    self.right_child = None
 
 
-    def get_children(self):
-        children = []
-        if self.left_child is not None:
-            children.append(self.left_child.data)
-        if self.right_child is not None:
-            children.append(self.right_child.data)
-        return children
-
-    def getdata(self, equipo1, equipo2):
-        if equipo1==True:
-            self.data=equipo1
-        if equipo2==True:
-            self.data=equipo2
-        return self.data 
-
-   
-    def __repr__(self):
-        return "Data: " + str(self.getdata(True, False)) + ", Children: " + str(self.get_children())
+  def get_children(self):
+    children = []
+    if self.left_child is not None:
+      children.append(self.left_child.data)
+    if self.right_child is not None:
+      children.append(self.right_child.data)
+    return children
 
 
-class BinaryTree:
-    def __init__(self):
-        self.root = None
-        self.left_child=None
-        self.right_child=None
+  def visualization(root):
+    current_level = [root]
+    while current_level:
+        print(' '.join(str(node) for node in current_level))
+        next_level = list()
+        for n in current_level:
+            if n.left_child:
+                next_level.append(n.left_child)
+            if n.right_child:
+                next_level.append(n.right_child)
+        current_level = next_level
+       
+
+  def __repr__(self):
+    return str(self.data)
+    #return str(self.data) + "\n" + str(self.get_children()) + "\n"
 
 
-    def insert(self, root, equipo1, equipo2):
-        if root is None:
-            self.root = Node(equipo1, equipo2)
+
+
+class BinarySearchTree:
+  def __init__(self):
+    self.root = None
+
+
+  def insert(self, root, value):
+    if root is None:
+      self.root = Nodes(value)
+      return
+
+    if root.data > value:
+      if root.left_child is None:
+        root.left_child = Nodes(value)
         return
-        if equipo1.winner == True:
-            if root.left_child is None:
-                root.left_child = Node(equipo1, equipo2)
-            return
-        else:
-            self.insert(root.left_child, equipo1, equipo2)
-        
-        if equipo2.winner == True:
-            if root.right_child is None:
-                root.right_child = Node(equipo1, equipo2)
-            return
-        else:
-            self.insert(root.right_child, equipo1, equipo2)
-
-
-    def inorder_traverse(self, root):
-        if root is not None:
-            self.inorder_traverse(root.left_child)
-        print(root.data)
-        self.inorder_traverse(root.right_child)
-
-        
-    def search(self, root, key):
-        if root is None:
-            return "Key not found in tree :("
-
-        if root.data == key:
-            return root
-
-        if root.data > key:
-            return self.search(root.left_child, key)
-        
-        if root.data < key: 
-            return self.search(root.right_child, key)
+      else:
+        self.insert(root.left_child, value)
     
-    def tree_size(self, teams):
-        size=teams.get_lastRank()
-        levels = int(log(size, 2))
-        return levels+1
+    if root.data < value:
+      if root.right_child is None:
+        root.right_child = Nodes(value)
+        return
+      else:
+        self.insert(root.right_child, value)
 
-    def create_nodes(self, size, lc, rc):
-        #Last Level
-        for i in range(size/2):
-            self.insert(self.root, 0,0)
+    
+  
+  def search(self, root, key):
+    if root is None:
+      return "not a team in this championship"
 
+    if root.data == key:
+      return root
+    
+    if root.data > key:
+      return self.search(root.left_child, key)
+    
+    if root.data < key: 
+      return self.search(root.right_child, key)
 
-    def find_min(self, root):
-        current_node = root
-
-        while current_node.left_child is not None:
-            current_node = current_node.left_child
-
-        return current_node
-
-
-    def find_max(self, root):
-        current_node = root
-
-        while current_node.right_child is not None:
-            current_node = current_node.right_child
-
-        return current_node
+  
