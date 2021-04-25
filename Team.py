be349e7740f0398e1b6b7afe1cd6141b93a96a28
marcelamelgar <61280126+marcelamelgar@users.noreply.github.com>
2021-04-25 13:21:46 -0600

    function

--- Team.py
+++ Team.py
@@ -23,3 +23,11 @@ class Team:
     def __str__(self):
         return self.name + "\nScore: " + str(self.score)
 
+    def finalScores(self, lista):
+
+        lista.sort(reverse=True)
+        print(lista)
+        return(lista)
+
+team1 = Team('brasil', 0)
+team1.finalScores([19,23,34,54,5,6,7,8,9,10,11,12])
